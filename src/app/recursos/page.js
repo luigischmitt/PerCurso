0a58928235a5dcfbd9fd3d72470349cea2814f5d
luigischmitt <schmittluigi@gmail.com>
2024-11-13 17:24:40 -0300

    feat: add banner image and update intro section styles

--- src/app/recursos/page.js
+++ src/app/recursos/page.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import styles from './page.module.css'; // Arquivo CSS para os estilos específicos da página
+import styles from './page.module.css';
 
 const PercursosPage = () => {
   return (
     <div>
       <main className={styles.main}>
         <section className={styles.introSection}>
+          <img src="./././girl_studying.svg" alt="Imagem ilustrativa do PerCurso" className={styles.bannerImage} />
           <div className={styles.introText}>
-            <h2 className={styles.title}>Materiais de Estudo</h2>
-            <p>
-              Para começar, basta escolher uma matéria e explorar os conteúdos e recursos disponíveis. Aqui você encontrará uma variedade de conteúdos, para facilitar sua revisão e aprofundamento nos temas abordados em aula. Aproveite todos os recursos para aprender de maneira eficiente e agradável!
+            <h2 className={styles.title}>📂 Materiais de Estudo</h2>
+            <p className={styles.title_p}>
+              Para começar, basta escolher uma matéria e explorar os <strong className={styles.accent}>conteúdos e recursos</strong> disponíveis. Aqui você encontrará uma variedade de conteúdos, para facilitar sua revisão e aprofundamento nos temas abordados em aula. Aproveite todos os recursos para <strong className={styles.accent}>aprender</strong> de maneira eficiente e agradável!
             </p>
           </div>
           <div className={styles.introText}>
-            <h2 className={styles.title}>Contribua com o PerCurso!</h2>
-            <p>
-              Tem materiais úteis, sugestões ou atividades para compartilhar? Você pode colaborar com o PerCurso enviando conteúdo que possa ajudar outros estudantes por meio do botão “editar” em cada disciplina. Seu apoio é muito bem-vindo para construir uma comunidade de aprendizado ainda mais rica!
+            <h2 className={styles.title}>✨ Contribua com o PerCurso!</h2>
+            <p className={styles.title_p}>
+              Tem materiais úteis, sugestões ou atividades para compartilhar? Você pode <strong className={styles.accent}>colaborar</strong> com o PerCurso enviando conteúdo que possa ajudar outros estudantes por meio do botão <strong className={styles.accent}>“editar”</strong> em cada disciplina.<strong className={styles.accent}>Seu apoio</strong> é muito bem-vindo para construir uma comunidade de aprendizado ainda mais rica!
             </p>
           </div>
         </section>
