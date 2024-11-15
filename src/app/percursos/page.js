27dce09f8ecd8be33d08ca91757a61f5348ec1f3
Nicolas Kleiton <inicolaskleiton@gmail.com>
2024-11-14 23:48:47 -0300

    fix: make logo button clickable by adjusting overlay issue

--- src/app/percursos/page.js
+++ src/app/percursos/page.js
@@ -10,7 +10,7 @@ const PercursosPage = () => {
           <p className={styles.description}>
             O sistema de percursos permite que você se especialize em uma área de interesse, ajudando a orientar suas escolhas de disciplinas optativas, ligas acadêmicas e laboratórios que mais se alinham ao tópico que deseja se especializar.
           </p>
-          <img src="././image2.svg" alt="Estudante olhando para o caminho" className={styles.image} />
+          <img src="././image2.svg" alt="Imagem ilustrativa do PerCurso" className={styles.image} />
           <p className={styles.subtitle}>
             Ao seguir um percurso, você se beneficia de uma orientação direcionada, aproveitando ao máximo o que o curso oferece e fazendo escolhas mais assertivas para atingir seus objetivos.
           </p>
