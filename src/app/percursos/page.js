a81efa205c02eed30caefb0bc387b846c8b7c256
luigischmitt <schmittluigi@gmail.com>
2024-11-20 18:29:32 -0300

    feat: add more labs to percurso's page

--- src/app/percursos/page.js
+++ src/app/percursos/page.js
@@ -12,25 +12,49 @@ const PercursosPage = () => {
       id: "tril",
       title: "TRILAB",
       description:
-        'A proposta do TRILAB, ou apenas TRIL, está baseada em uma tríade conceitual que dá origem ao seu nome: SEIZE, SHAPE e SOLVE. Este modelo holístico “3S” tem como objetivo implementar um portfólio de soluções para setores estratégicos da nossa economia e sociedade.',
+        'A proposta do TRILAB, ou apenas TRIL, está baseada em uma tríade conceitual que dá origem ao seu nome: SEIZE, SHAPE e SOLVE. Este modelo holístico “3S” tem como objetivo implementar um portfólio de soluções para setores estratégicos da nossa economia e sociedade, atuando em horizontes de pesquisa que perpassam tópicos interdisciplinares.',
       imgSrc: "/tril_logo.svg",
       link: "https://tril.ci.ufpb.br/en/",
     },
     {
-      id: "tail",
-      title: "TAIL",
+      id: "lavid",
+      title: "LAVID",
       description:
-        "Technology and Artificial Intelligence League, ou simplesmente TAIL, incrementa a bagagem intelectual de seus associados nos mais diversos assuntos da área de dados e no ramo da Inteligência Artificial.",
-      imgSrc: "/tail_logo.svg",
-      link: "https://tail-tech.com/",
+        "O LAVID surgiu da proposta de desenvolver projetos de pesquisa e desenvolvimento em hardware e software voltados para plataformas de sistemas e aplicações multimídia. Atualmente, o LAVID possui 18 pesquisadores docentes credenciados, mais de 150 alunos pesquisadores e 9 patentes registradas nas suas áreas de atuação.",
+      imgSrc: "/lavid_logo.svg",
+      link: "http://lavid.ufpb.br/",
     },
     {
-      id: "mccufpb",
-      title: "MCCUFPB",
+      id: "aria",
+      title: "ARIA",
       description:
-        "Meninas na Ciência da Computação busca incentivar a participação feminina em áreas tecnológicas, promovendo inclusão e capacitação em Ciência da Computação.",
-      imgSrc: "/mcc_logo.svg",
-      link: "https://mccufpb.ufpb.br/",
+        "O ARIA (ARtificial Intelligence Applications laboratory) acredita que as experiências definem a aprendizagem. Assim, o ARIA constrói experiências para máquinas e para pessoas, formando especialistas na área de inteligência artificial e ciência de dados, desenvolvendo aplicações e pesquisando seus métodos.",
+      imgSrc: "/aria_logo.svg",
+      link: "https://aria.ci.ufpb.br/",
+    },
+    {
+      id: "laser",
+      title: "LASER",
+      description:
+        "Criado em 2013, o Laboratório de Engenharia de Sistemas e Robótica - LASER surgiu da proposta de desenvolver projetos de pesquisa em hardware e software voltados às áreas de Robótica, Sistemas Embarcados e Inteligência Artificial. Hoje ele se destaca por projetos de excelência nas áreas, Robótica Móvel, Sistemas Embarcados e Projeto de Hardware, Otimização e Logística.",
+      imgSrc: "/laser_logo.svg",
+      link: "https://sites.google.com/view/laser-ufpb/",
+    },
+    {
+      id: "lmi",
+      title: "LMI",
+      description:
+        "O Laboratório de Medidas e Instrumentação (LMI) tem o intuito de desenvolver pesquisas na área de eletrônica, microeletrônica aplicadas no uso de sensores e atuadores. Fazem parte do escopo das pesquisas desenvolver novos sensores bem como aplicações ligadas aos conceitos: smart-home, tecnologia assistiva, reabilitação virtual e monitoramento remoto.",
+      imgSrc: "/lmi_logo.svg",
+      link: "https://lmi.ci.ufpb.br/",
+    },
+    {
+      id: "lasid",
+      title: "LASID",
+      description:
+        "O Laboratório de Sistemas Digitais (LASID) tem a proposta de realizar pesquisas em desenvolvimento de hardware, sistemas embarcados e processamento de sinais. Também está ligado ao Programa de Pós Graduação em Informática (PPGI) e já participou de diversos projetos, ganhando conhecimento, experiência e infra-estrutura.",
+      imgSrc: "/lasid_logo.svg",
+      link: "http://www.lasid.di.ufpb.br/",
     },
   ];
 
@@ -48,8 +72,8 @@ const PercursosPage = () => {
 
   return (
   <div>
+    <div className={styles.lineTop}></div> 
     <div className={styles.backgroundRectangle}>
-      <div className={styles.lineMid}></div>
       <div className={`${styles.container} ${styles.percursos}`}>
         <div className={styles.heroText}>
           <h2 className={styles.sectionTitle}>PERCURSOS</h2>
@@ -111,39 +135,41 @@ const PercursosPage = () => {
       </Link>
     </div>
 
-    <section className={styles.laboratorios}>
-        <h2 className={styles.sectionTitle2}>LABORATÓRIOS</h2>
-        <div className={styles.carousel}>
-          <button
-            className={`${styles.carouselButton} ${styles.left}`}
-            onClick={handleLabPrev}
-          >
-            &lt;
-          </button>
-          <div className={styles.card2}>
+    <div className={styles.lineTop}></div>
+    <div className={styles.laboratorios}>
+      <h2 className={styles.sectionTitle2}>LABORATÓRIOS</h2>
+      <div className={styles.carouselContainer}>
+        <button
+          className={`${styles.carouselButton} ${styles.left}`}
+          onClick={handleLabPrev}
+        >
+          &lt;
+        </button>
+        <div className={styles.labCard}>
+          <a href={labs[labIndex].link} target="_blank" rel="noopener noreferrer">
+            <img
+              src={labs[labIndex].imgSrc}
+              alt={`Logo ${labs[labIndex].title}`}
+              className={styles.cardImage}
+            />
+          </a>
+          <h3 className={styles.cardTitle}>{labs[labIndex].title}</h3>
+          <p className={styles.cardTextlab}>{labs[labIndex].description}</p>
+          <p className={styles.cardLink}>
             <a href={labs[labIndex].link} target="_blank" rel="noopener noreferrer">
-              <img
-                src={labs[labIndex].imgSrc}
-                alt={`Logo ${labs[labIndex].title}`}
-                className={styles.cardImage}
-              />
+              Clique para saber mais!
             </a>
-            <h3 className={styles.cardTitle}>{labs[labIndex].title}</h3>
-            <p className={styles.cardText}>{labs[labIndex].description}</p>
-            <p className={styles.cardLink}>
-              <a href={labs[labIndex].link} target="_blank" rel="noopener noreferrer">
-                Clique para saber mais!
-              </a>
-            </p>
-          </div>
-          <button
-            className={`${styles.carouselButton} ${styles.right}`}
-            onClick={handleLabNext}
-          >
-            &gt;
-          </button>
+          </p>
         </div>
-      </section>
+        <button
+          className={`${styles.carouselButton} ${styles.right}`}
+          onClick={handleLabNext}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+    <div className={styles.lineTop}></div>
 
   </div>
   );
