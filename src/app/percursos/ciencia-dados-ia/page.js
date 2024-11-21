0702fa0c4c343bb52b1aca455421c020e61c3341
bea <beatrizamgpessoa@gmail.com>
2024-11-21 15:36:58 -0300

    style: changed details on cdia page

--- src/app/percursos/ciencia-dados-ia/page.js
+++ src/app/percursos/ciencia-dados-ia/page.js
@@ -26,12 +26,15 @@ export default function Page() {
               </div>
             </div>
             <aside className={styles.labs}>
-              <h3>Laboratórios & Ligas</h3>
-              <ul>
-                <li>TAIL</li>
-                <li>TRIL</li>
-                <li>ARIA</li>
-              </ul>
+              <div className={styles.lineVertical}></div>
+              <div className={styles.labsText}>
+                <h3>Laboratórios & Ligas</h3>
+                <ul>
+                  <li>TAIL</li>
+                  <li>TRIL</li>
+                  <li>ARIA</li>
+                </ul>
+              </div>
             </aside>
           </header>
         </div>
@@ -40,72 +43,76 @@ export default function Page() {
 
       <section className={styles.map}>
         <img
-          src="/cdia_roadmap.svg"
+          src="/cdia_roadmap2.svg"
           alt="Mapa de Disciplinas"
           className={styles.mapImage}
         />
       </section>
 
-      <div className={styles.container}>
-        <section className={styles.courses}>
-          <h2 className={styles.subtitle}>DISCIPLINAS</h2>
-          <div className={styles.courseCards}>
-            {[
-              {
-                codigo: "GDCOC0096",
-                nome: "VISUALIZAÇÃO DE DADOS",
-                tipo: "Optativa",
-                periodo: 4,
-                descricao: "Ferramentas e técnicas para transformar dados complexos em gráficos e visualizações interativas, facilitando sua interpretação e análise.",
-              },
-              {
-                codigo: "GDCOC0114",
-                nome: "SÉRIES TEMPORAIS",
-                tipo: "Optativa",
-                periodo: 4,
-                descricao: "Análise de dados organizados em ordem cronológica para identificar padrões, tendências e previsões futuras.",
-              },
-              {
-                codigo: "GDSCO0032",
-                nome: "TEORIA DOS GRAFOS APLICADA",
-                tipo: "Optativa",
-                periodo: 4,
-                descricao: "Estudo de grafos para resolver problemas em áreas como redes sociais, rotas logísticas e análise de conectividade",
-              },
-              {
-                codigo: "DINF00068",
-                nome: "INTELIGÊNCIA ARTIFICIAL APLICADA À SAÚDE",
-                tipo: "Optativa",
-                periodo: 5,
-                descricao: "Uso de algoritmos de IA para diagnósticos, otimização de tratamentos, e avanços na medicina personalizada.",
-              },
-              {
-                codigo: "GDCOC0098",
-                nome: "BIG DATA",
-                tipo: "Optativa",
-                periodo: 6,
-                descricao: "Conjunto de tecnologias para processar e analisar grandes volumes de dados variados, gerando insights estratégicos.",
-              },
-              {
-                codigo: "GDCOC0094",
-                nome: "APRENDIZADO PROFUNDO",
-                tipo: "Optativa",
-                periodo: 7,
-                descricao: "Subcampo da IA que utiliza redes neurais profundas para resolver problemas complexos, como reconhecimento de imagens e processamento de linguagem natural.",
-              },
-                        
-            ].map((disciplina, index) => (
-              <div className={styles.card} key={index}>
-                <h3>{disciplina.codigo}</h3>
-                <p>{disciplina.nome}</p>
-                <p><strong>{disciplina.tipo}</strong></p>
-                <p>Período: {disciplina.periodo}</p>
-                <p>{disciplina.descricao}</p>
-                </div>
-            ))}
-          </div>
-        </section>
+      <div className={styles.backgroundRectangle2}>
+        <div className={styles.lineMid}></div> 
+        <div className={styles.container}>
+          <section className={styles.courses}>
+            <h2 className={styles.subtitle}>DISCIPLINAS</h2>
+            <div className={styles.courseCards}>
+              {[
+                {
+                  codigo: "GDCOC0096",
+                  nome: "VISUALIZAÇÃO DE DADOS",
+                  tipo: "Optativa",
+                  periodo: 4,
+                  descricao: "Ferramentas e técnicas para transformar dados complexos em gráficos e visualizações interativas, facilitando sua interpretação e análise.",
+                },
+                {
+                  codigo: "GDCOC0114",
+                  nome: "SÉRIES TEMPORAIS",
+                  tipo: "Optativa",
+                  periodo: 4,
+                  descricao: "Análise de dados organizados em ordem cronológica para identificar padrões, tendências e previsões futuras.",
+                },
+                {
+                  codigo: "GDSCO0032",
+                  nome: "TEORIA DOS GRAFOS APLICADA",
+                  tipo: "Optativa",
+                  periodo: 4,
+                  descricao: "Estudo de grafos para resolver problemas em áreas como redes sociais, rotas logísticas e análise de conectividade",
+                },
+                {
+                  codigo: "DINF00068",
+                  nome: "INTELIGÊNCIA ARTIFICIAL APLICADA À SAÚDE",
+                  tipo: "Optativa",
+                  periodo: 5,
+                  descricao: "Uso de algoritmos de IA para diagnósticos, otimização de tratamentos, e avanços na medicina personalizada.",
+                },
+                {
+                  codigo: "GDCOC0098",
+                  nome: "BIG DATA",
+                  tipo: "Optativa",
+                  periodo: 6,
+                  descricao: "Conjunto de tecnologias para processar e analisar grandes volumes de dados variados, gerando insights estratégicos.",
+                },
+                {
+                  codigo: "GDCOC0094",
+                  nome: "APRENDIZADO PROFUNDO",
+                  tipo: "Optativa",
+                  periodo: 7,
+                  descricao: "Subcampo da IA que utiliza redes neurais profundas para resolver problemas complexos, como reconhecimento de imagens e processamento de linguagem natural.",
+                },
+                          
+              ].map((disciplina, index) => (
+                <div className={styles.card} key={index}>
+                  <p>{disciplina.codigo}</p>
+                  <h3>{disciplina.nome}</h3>
+                  <p><strong>{disciplina.tipo}</strong></p>
+                  <p>Período: {disciplina.periodo}</p>
+                  <p>{disciplina.descricao}</p>
+                  </div>
+              ))}
+            </div>
+          </section>
+        </div>
       </div>
+
       <footer className={styles.footer}>
         <p>© 2024 PerCurso, Todos os direitos reservados</p>
         <a href="https://github.com/luigischmitt/PerCurso/blob/main/C%C3%B3digo_de_conduta.md" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>Código de Conduta</a>
