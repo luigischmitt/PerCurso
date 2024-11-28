8611c41c64bbaf00a9e6b1438a9c25dcea923572
luigischmitt <schmittluigi@gmail.com>
2024-11-28 00:33:52 -0300

    style: improve responsiveness of recurso's page

--- src/app/recursos/[disciplina]/page.js
+++ src/app/recursos/[disciplina]/page.js
@@ -151,7 +151,7 @@ export default function DisciplinaPage({ params }) {
   const disciplinaIcone = disciplinaIcones[params.disciplina] || "Icone";
 
   return (
-    <div>
+    <div className={styles.pageWrapper}>
       <div className={styles.backgroundRectangle}>
         <div className={styles.lineTop}></div>
         <div className={styles.container}>
@@ -173,7 +173,6 @@ export default function DisciplinaPage({ params }) {
         <div className={styles.lineBottom}></div>
       </div>
       <div className={styles.container}>
-        {/* Use o componente DisciplinaContent para o conteúdo interativo */}
         <DisciplinaContent
           dicas={dicas}
           dificuldades={dificuldades}
@@ -182,7 +181,10 @@ export default function DisciplinaPage({ params }) {
           extras={extras}
         />
       </div>
+      <footer className={styles.footer}>
+        <p>© 2024 PerCurso, Todos os direitos reservados</p>
+        <a href="https://github.com/luigischmitt/PerCurso/blob/main/C%C3%B3digo_de_conduta.md" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>Código de Conduta</a>
+      </footer>
     </div>
   );
 }
-
