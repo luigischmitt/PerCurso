96f07d1a42de5bf31b9c7d797f72823d94201d01
luigischmitt <schmittluigi@gmail.com>
2024-11-28 17:27:19 -0300

    styçe: add viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link"; // Importa o Link do Next.js
 import styles from './page.module.css'; // Importa o CSS do page.js
+import Head from 'next/head'; // Importa o Head do Next.js
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
         <header className={styles.header}>
           <Link href="/" passHref>
             <h1 style={{ fontFamily: 'Lemon, sans-serif', marginRight: '15px', cursor: 'pointer' }} className={styles.logo}>PerCurso</h1>
